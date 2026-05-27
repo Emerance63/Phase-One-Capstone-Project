b29368c143dc198996846dc2ccec1dd5be4f5089
Emerance63 <iribagizaemerance63@gmail.com>
2026-05-27 11:22:52 +0200

    fix the desplay of humbergur-menu list

--- Asset/humbergur-menu.js
+++ Asset/humbergur-menu.js
@@ -3,4 +3,12 @@ const navLinks = document.querySelector('.nav-links');
 
 hamburgerMenu.addEventListener('click', () => {
   navLinks.classList.toggle('hidden');
+  navLinks.classList.toggle('mobile-open');
+});
+
+document.addEventListener('click', (e) => {
+  if (!hamburgerMenu.contains(e.target) && !navLinks.contains(e.target)) {
+    navLinks.classList.add('hidden');
+    navLinks.classList.remove('mobile-open');
+  }
 });
